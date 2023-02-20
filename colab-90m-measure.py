69623a935f38e1e63bf9ce878ef4f20a4263ef5b
kitsune_yk <agvksl543@gmail.com>
2023-02-20 09:24:43 +0900

    colabの90分対策

---
+++ colab-90m-measure.py
@@ -0,0 +1,10 @@
+import time
+import datetime
+import webbrowser
+
+# 1時間毎に任意のノートブックを開く
+for i in range(12):
+    browse = webbrowser.get('chrome')
+    browse.open('https://colab.research.google.com/drive/1r4b_wGBmYUuG4oL5pOI4S7wwgiymAGZ3#scrollTo=bEVPRiXZBliZ')
+    print(i, datetime.datetime.today())
+    time.sleep(60*60)
